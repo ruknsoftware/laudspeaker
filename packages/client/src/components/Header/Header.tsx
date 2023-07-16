7d7d0485ffb3aaf829a88f01262de4432ef4af6e
devmuhnnad <muhnnadhabib@gmail.com>
2023-07-16 10:22:51 +0300

    Done split features for onboarding page

--- packages/client/src/components/Header/Header.tsx
+++ packages/client/src/components/Header/Header.tsx
@@ -11,6 +11,7 @@ import { Link } from "react-router-dom";
 import { useAppSelector } from "store/hooks";
 import { useNavigate } from "react-router-dom";
 import React from "react";
+import RenderWhenOn from "components/splitFeature/RenderWhenOn";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -101,20 +102,22 @@ const Header: FC<HeaderProps> = ({ crumbs }) => {
               leaveTo="transform opacity-0 scale-95"
             >
               <Menu.Items className="absolute z-[120] right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <Menu.Item>
-                  {({ active }) => (
-                    <Link to="/settings">
-                      <div
-                        className={classNames(
-                          active ? "bg-gray-100" : "",
-                          "block px-4 py-2 text-sm text-gray-700"
-                        )}
-                      >
-                        Settings
-                      </div>
-                    </Link>
-                  )}
-                </Menu.Item>
+                <RenderWhenOn featureName="settings">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link to="/settings">
+                        <div
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          Settings
+                        </div>
+                      </Link>
+                    )}
+                  </Menu.Item>
+                </RenderWhenOn>
                 <Menu.Item>
                   {({ active }) => (
                     <div
